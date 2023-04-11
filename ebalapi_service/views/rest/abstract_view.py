0311f9199337ff3b773917dbca3f101168983c26
Murphy <thehelixpg@gmail.com>
2023-04-11 15:50:52 +0300

    update api views

--- ebalapi_service/views/rest/abstract_view.py
+++ ebalapi_service/views/rest/abstract_view.py
@@ -1,9 +1,11 @@
 from django.core import exceptions
 from django.db.models import QuerySet
+from django.utils.decorators import method_decorator
 from rest_framework import exceptions as rest_exception, mixins
 from rest_framework import generics
 from rest_framework import serializers
 from rest_framework.authentication import BasicAuthentication, TokenAuthentication
+from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
@@ -12,44 +14,111 @@ from rest_framework.response import Response
 from .token_auth import GetTokenAuthentication
 
 
+# class AbstractCRUDView(generics.RetrieveUpdateDestroyAPIView,
+#                        mixins.CreateModelMixin):
+#
+#     authentication_classes = [
+#         # SessionAuthentication,
+#         BasicAuthentication,
+#         TokenAuthentication,
+#         GetTokenAuthentication
+#     ]
+#     permission_classes = [IsAuthenticated]
+#     serializer_class: serializers.ModelSerializer
+#     queryset: QuerySet
+#
+#     def post(self, request: Request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+#
+#     def get_object(self):
+#         """
+#         Returns the object the view is displaying.
+#         """
+#         try:
+#             queryset = self.filter_queryset(self.get_queryset())
+#             query_params = self.request.query_params.dict()
+#
+#             if isinstance(self.request.data, dict):
+#                 query_params.update(**self.request.data)
+#             if not query_params:
+#                 raise rest_exception.NotFound
+#             if self.request.method in ['PUT', 'PATCH']:
+#                 obj_id = query_params.get('id')
+#                 filter_kwargs = {"id": obj_id}
+#             obj = get_object_or_404(queryset, **query_params)
+#         except exceptions.FieldError:
+#             raise rest_exception.NotFound
+#         # May raise a permission denied
+#         self.check_object_permissions(self.request, obj)
+#
+#         return obj
+
+
 class AbstractCRUDView(generics.RetrieveUpdateDestroyAPIView,
                        mixins.CreateModelMixin):
 
-    authentication_classes = [
-        # SessionAuthentication,
-        BasicAuthentication,
-        TokenAuthentication,
-        GetTokenAuthentication
-    ]
-    permission_classes = [IsAuthenticated]
-    serializer_class: serializers.ModelSerializer
-    queryset: QuerySet
+    serializer_class = serializers.ModelSerializer
+    queryset = QuerySet
 
-    def post(self, request: Request, *args, **kwargs):
+    @method_decorator(authentication_classes([BasicAuthentication, TokenAuthentication, GetTokenAuthentication]))
+    @method_decorator(permission_classes([IsAuthenticated]))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
     def get_object(self):
-        """
-        Returns the object the view is displaying.
-        """
-        try:
-            queryset = self.filter_queryset(self.get_queryset())
-            query_params = self.request.query_params.dict()
 
-            if isinstance(self.request.data, dict):
-                query_params.update(**self.request.data)
-            if not query_params:
-                raise rest_exception.NotFound
-            if self.request.method in ['PUT', 'PATCH']:
-                obj_id = query_params.get('id')
-                filter_kwargs = {"id": obj_id}
-            obj = get_object_or_404(queryset, **query_params)
-        except exceptions.FieldError:
-            raise rest_exception.NotFound
-        # May raise a permission denied
+        queryset = self.filter_queryset(self.get_queryset())
+        query_params = self.request.query_params.dict()
+
+        # Get URL path or path list
+        # path = self.request.path
+        path_list = list(filter(None, self.request.path.split('/')))
+
+        if isinstance(self.request.data, dict):
+            query_params.update(**self.request.data)
+        if path_list[-1].isnumeric():
+            query_params.update(id=path_list[-1])
+        elif not query_params:
+            raise rest_exception.NotFound("Query parameters not found")
+
+        obj_id = self.request.data.get('id')
+        filter_kwargs = {"id": obj_id} if obj_id else {}
+
+        obj = get_object_or_404(queryset, **query_params, **filter_kwargs)
+
         self.check_object_permissions(self.request, obj)
 
         return obj
+
+
+# class AbstractListItemView(generics.ListAPIView):
+#     authentication_classes = [
+#         # SessionAuthentication,
+#         BasicAuthentication,
+#         TokenAuthentication,
+#         GetTokenAuthentication
+#     ]
+#     permission_classes = [IsAuthenticated]
+#     serializer_class: serializers.ModelSerializer
+#     queryset: QuerySet
+#
+#     def get(self, request, *args, **kwargs):
+#         query_params = request.query_params.dict()
+#         if isinstance(request.data, dict):
+#             query_params.update(**request.data)
+#         try:
+#             items = self.queryset.filter(**query_params)
+#             serialized = self.serializer_class(items, many=True)
+#             content = {
+#                 "totalItems": len(serialized.data),
+#                 "items": serialized.data
+#             }
+#             return Response(content)
+#         except exceptions.FieldError:
+#             raise rest_exception.NotFound
 
 
 class AbstractListItemView(generics.ListAPIView):
@@ -60,20 +129,21 @@ class AbstractListItemView(generics.ListAPIView):
         GetTokenAuthentication
     ]
     permission_classes = [IsAuthenticated]
-    serializer_class: serializers.ModelSerializer
-    queryset: QuerySet
+    serializer_class = serializers.ModelSerializer
+    queryset = QuerySet
+
+    def get_queryset(self):
+        queryset = self.queryset.all()
+        query_params = self.request.query_params
+        if isinstance(self.request.data, dict):
+            query_params.update(self.request.data)
+        return queryset.filter(**query_params)
 
     def get(self, request, *args, **kwargs):
-        query_params = request.query_params.dict()
-        if isinstance(request.data, dict):
-            query_params.update(**request.data)
-        try:
-            items = self.queryset.filter(**query_params)
-            serialized = self.serializer_class(items, many=True)
-            content = {
-                "totalItems": len(serialized.data),
-                "items": serialized.data
-            }
-            return Response(content)
-        except exceptions.FieldError:
-            raise rest_exception.NotFound
+        queryset = self.get_queryset()
+        serialized = self.serializer_class(queryset, many=True)
+        content = {
+            "totalItems": queryset.count(),
+            "items": serialized.data
+        }
+        return Response(content)
--- ebalapi_service/web/urls.py
+++ ebalapi_service/web/urls.py
@@ -12,25 +12,31 @@ urlpatterns = [
 
     path('dragfuncs/', views.rest.DragFunctionView.as_view()),
     path('dragfunc/', views.rest.DragFunctionCRUDView.as_view()),
+    path('dragfunc/<int:id>/', views.rest.DragFunctionCRUDView.as_view(), name='dragfunction-detail'),
 
     path('diameters/', views.rest.DiameterView.as_view()),
     # path('diameter/', views.rest.DiameterCRUDView.as_view()),
 
-    path('calibers/', views.rest.CaliberView.as_view(), name='caliber-list'),
-    path('caliber/', views.rest.CaliberCRUDView.as_view(), name='caliber-detail'),
-    path('caliber/<int:id>/', views.rest.CaliberDetailView.as_view(), name='caliber-detail'),
+    path('calibers/', views.rest.CaliberView.as_view()),
+    path('caliber/', views.rest.CaliberCRUDView.as_view()),
+    path('caliber/<int:id>/', views.rest.CaliberCRUDView.as_view(), name='caliber-detail'),
+    # path('caliber/<int:id>/', views.rest.CaliberDetailView.as_view(), name='caliber-detail'),
 
     path('vendors/', views.rest.VendorView.as_view()),
     path('vendor/', views.rest.VendorCRUDView.as_view()),
-    path('vendor/<int:id>/', views.rest.VendorDetailView.as_view(), name='vendor-detail'),
+    path('vendor/<int:id>/', views.rest.CaliberCRUDView.as_view(), name='vendor-detail'),
+    # path('vendor/<int:id>/', views.rest.VendorDetailView.as_view(), name='vendor-detail'),
 
     path('rifles/', views.rest.RifleView.as_view()),
     path('rifle/', views.rest.RifleCRUDView.as_view()),
+    path('rifle/<int:id>/', views.rest.RifleCRUDView.as_view(), name='rifle-detail'),
 
     path('bullets/', views.rest.BulletView.as_view()),
     path('bullet/', views.rest.BulletCRUDView.as_view()),
+    path('bullet/<int:id>/', views.rest.BulletCRUDView.as_view(), name='bullet-detail'),
 
     path('cartridges/', views.rest.CartridgeView.as_view()),
     path('cartridge/', views.rest.CartridgeCRUDView.as_view()),
+    path('cartridge/<int:id>/', views.rest.CartridgeCRUDView.as_view(), name='cartridge-detail'),
 
 ]
